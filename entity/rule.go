a9ff76e1add43a695a29a51253eb88f51acd192f
sepehrhakimi90 <sepehrhakimi90@gmail.com>
2021-05-28 15:36:03 +0430

    Add Suspecr filed in rule

--- entity/rule.go
+++ entity/rule.go
@@ -8,7 +8,8 @@ type Rule struct {
 	ID        uint `gorm:"primarykey;autoIncrement"`
 	CreatedAt time.Time
 	StartTime time.Time `gorm:"not null" json:"start_time"`
-	EndTime   int64 `gorm:"not null"`
-	Domain    string `gorm:"not null" json:"domain"`
-	TTL       int `gorm:"not null" json:"ttl"`
+	Suspect   string    `gorm:"not null" json:"suspect"`
+	EndTime   int64     `gorm:"not null"`
+	Domain    string    `gorm:"not null" json:"domain"`
+	TTL       int       `gorm:"not null" json:"ttl"`
 }
